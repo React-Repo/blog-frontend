73ebdf03b9c627c68e432d3d74a09b0927c30947
sunyeongchoi <sn0716@naver.com>
2021-05-20 00:35:30 +0900

    페이지네이션 구현중. 리스트쪽에서 에러발생

--- src/pages/PostListPage.js
+++ src/pages/PostListPage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import HeaderConatiner from '../containers/common/HeaderConatiner';
 import PostListContainer from '../containers/posts/PostListContainer';
+import PaginationContainer from '../containers/posts/PaginationContainer';
 
 const PostListPage = () => {
   return (
     <>
       <HeaderConatiner />
       <PostListContainer />
+      <PaginationContainer />
     </>
   );
 };
